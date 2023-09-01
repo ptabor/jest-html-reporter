9df704df7cb825a2cf58e5b63f671db323869a9f
Piotr Tabor <piotr.tabor@snowflake.com>
2023-09-01 17:35:27 +0200

    Shorten the tmp path that jest is generating.
    Error: An error occurred while adding the reporter at path "/private/var/tmp/_bazel_ptabor/7822fa32d222a4d7f0cf5a703ea68dac/sandbox/darwin-sandbox/2035/execroot/sfc_snapps/bazel-out/darwin_arm64-fastbuild/bin/src/[redacted]/.aspect_rules_js/jest-html-reporters@3.1.4/node_modules/jest-html-reporters/index.js".
    ENAMETOOLONG: name too long, unlink '/var/folders/p3/x51cvt2j2dj9cb7rrq2pzg_80000gn/T/L3ByaXZhdGUvdmFyL3RtcC9fYmF6ZWxfcHRhYm9yLzc4MjJmYTMyZDIyMmE0ZDdmMGNmNWE3MDNlYTY4ZGFjL3NhbmRib3gvZGFyd2luLXNhbmRib3gvMjAzNS9leGVjcm9vdC9zZmNfc25hcHBzL2JhemVsLW91dC9kYXJ3aW5fYXJtNjQtZmFzdGJ1aWxkL2Jpbi9zcmMvW3JlZGFjdGVkLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLi4uLnJlZGFjdGVkXQo/jest-html-reporters-temp']
    
    This problem is especially likely to happen when using on Bazel (that uses long paths).

--- helper.ts
+++ helper.ts
@@ -1,6 +1,7 @@
 import fs from 'fs-extra';
 import os from 'os';
 import path from 'path';
+import{ createHash } from 'crypto';
 
 let username: string = '';
 try {
@@ -11,7 +12,7 @@ try {
 
 export const tempDirPath = path.resolve(
   process.env.JEST_HTML_REPORTERS_TEMP_DIR_PATH || os.tmpdir(),
-  `${username}-${Buffer.from(process.cwd()).toString('base64')}`,
+  `${username}-${createHash('sha256').update(Buffer.from(process.cwd())).digest('base64')}`,
   'jest-html-reporters-temp'
 );
 
